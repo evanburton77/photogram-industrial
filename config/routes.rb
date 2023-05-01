1261e4c434ab7ceb07a164e1fc86facda7e6a8f8
Evan Burton <79816378+evanburton77@users.noreply.github.com>
2023-05-01 20:13:34 +0000

    added commenting

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :comments
   resources :photos
-  resources :users, only: :show
+
   devise_for :users
+
+  get "/:username" => "users#show"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
