82c900b35dde7ab0d2c01ff5f8e57cd45b5ecd78
Evan Burton <79816378+evanburton77@users.noreply.github.com>
2023-05-01 16:11:55 +0000

    added CDN (bootstrap) Assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  root "photos#index"
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
   devise_for :users
 
-  root "photos#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
