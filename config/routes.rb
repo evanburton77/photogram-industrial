ed3e55df420f261cba300235c8169543d26c243e
Evan Burton <79816378+evanburton77@users.noreply.github.com>
2023-05-01 19:07:44 +0000

    setup user show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root "photos#index"
+
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
+  resources :users, only: :show
   devise_for :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
